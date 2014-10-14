610f2e6eff762a014d520e1d4a9665bd5c570417
Mitchell Wulfman <mitchwulfman@gmail.com>
2014-10-14 18:00:47 -0400

    update addPackages

--- addPackages.js
+++ addPackages.js
@@ -1,12 +1,4 @@
-// replace the contents of packages.js in .meteor with the below, then delete this file
+// TODO: replace the contents of packages.js in .meteor with the below, then delete this file
 
-iron:core
 iron:router
-iron:controller
-iron:dynamic-template
-iron:layout
-iron:middleware-stack
-iron:url
-iron:location
 stylus
-bootstrap
